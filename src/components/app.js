7260e7d0b419d0094c27539a0f8d6ebb512354c8
Mac <mac@MacBook-Pro.local>
2018-05-16 14:49:18 -0600

    setup birthday form component, setup datepicker with moment

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import BirthdayForm from './birthday_form';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <BirthdayForm />
       </div>
     );
   }
