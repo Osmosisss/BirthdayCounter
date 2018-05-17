1e1c2aa36ecfe61d082fb10f52a26327973ad4d9
Mac <mac@MacBook-Pro.local>
2018-05-17 11:16:48 -0600

    added conditions and logic to calculate countdown correctly

--- src/components/clock.js
+++ src/components/clock.js
@@ -19,7 +19,24 @@ class Clock extends Component {
     getTimeRemaining(birthday) {
 
         var bday = new Date(birthday);
-        let today = new Date();
+        var today = new Date();
+
+        const currentMonth = today.getMonth();
+        const birthMonth = bday.getMonth();
+
+        if (birthMonth > currentMonth) {
+            bday.setFullYear(today.getFullYear());
+        } else if (birthMonth < currentMonth) {
+            bday.setFullYear(today.getFullYear() + 1);
+        } else if (birthMonth == currentMonth) {
+            const birthDay = bday.getDate();
+            if (birthMonth > currentMonth) {
+                bday.setFullYear(today.getFullYear());
+            }
+            else if (birthMonth < currentMonth) {
+                bday.setFullYear(today.getFullYear() + 1);
+            }
+        }
 
         var distance = bday.getTime() - today.getTime();
 
